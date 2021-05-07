dc1ea4c30e98392f6220b108ecd936fa24537fd2
ahmad jury <ahmadjury12@gmail.com>
2021-05-07 21:35:18 +0300

    loops_basic1

---
+++ _python/python_fundamentals/For_loop_Basic1/For_Loops_BasicI.py
@@ -0,0 +1,31 @@
+for x in range(151):
+    print(x)
+
+for x in range(5,1001,5):
+    print(x)    
+
+for x in range(100):
+    if x % 10 == 0:
+        print("coding Dojo") 
+    elif x % 5 == 0:
+        print ("coding") 
+    else:
+        print(x)
+
+cont=0
+for x in range (500000):
+    if x % 2 == 0:
+        cont=cont+1
+print(cont) 
+        
+
+for x in range (2018,0,-4):
+    print(x)
+
+lowNum = 2
+highNum = 9
+mult = 3
+for i in range (lowNum, highNum+1):
+    if i % mult == 0:
+        print (i)
+
